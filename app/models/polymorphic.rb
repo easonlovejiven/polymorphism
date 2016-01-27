bad3e01b0b76d9c507254b04eb4a302db17a9a8e
yuanjianhe <jianhe.yuan@weizhuli.com>
2016-01-27 19:53:50 +0800

    多态详细介绍

--- app/models/polymorphic.rb
+++ app/models/polymorphic.rb
@@ -22,3 +22,28 @@
 # 在 Picture 模型的实例上，可以使用 @picture.imageable 
 # 获取父对象。不过事先要在声明多态接口的模型中创建外键字段和类型字段
 # ［要提前声明imageable_id,imageable_type］
+
+# 多态第二种应用场景
+# 课程：course
+# 学生： student
+# 老师：teacher
+# 学生可以选择不同的课程，一个课程可以被多个学生选择
+# 老师对应多个课程，一个课程只能被一个老师选择
+# 总结:学生和课程之间是多对多,老师和课程之间是一对多,课程和老师之间是多对一
+
+# (1)Student 表字段：只有一个学生name
+# class Student < ActiveRecord::Base
+# 	has_many :courses, as: :courseable
+# end
+
+# (2)Teacher 表字段：只有一个老师name
+# class Teacher < ActiveRecord::Base
+# 	has_many :courses, as: :courseable
+# end
+
+# (3)Course 它作为一个多态接口 表字段：课程name,外键id,外键type(这儿的一条记录可以反映出：这个课程被哪个老师教或者这个学生所选择的课程)
+# class Course < ActiveRecord::Base
+# 	belongs_to :imageable, polymorphic: true
+# end
+
+# 可以得到:(1)student.courses, (2)teacher.courses, (3)course.imageable可以获取父对象（要在多态接口上声明外键的字段id和type）
